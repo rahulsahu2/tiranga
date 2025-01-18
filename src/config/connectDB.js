02ae9a7623e676d1819c72ea307ba9594b77d94e
Rahul Sahu <rahulsa667@gmail.com>
2025-01-18 23:56:47 +0530

    db update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -7,18 +7,18 @@ import mysql from 'mysql2/promise';
 //     database: 'tiranga',
 // });
 
-// const connection = mysql.createPool({
-//     host: '193.203.184.165',
-//     user: 'u936826252_daddy',
-//     password: 'H@>M=5>aP5',
-//     database: 'u936826252_daddy',
-// });
-
 const connection = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'my_database',
+    host: '193.203.184.165',
+    user: 'u936826252_daddy',
+    password: 'H@>M=5>aP5',
+    database: 'u936826252_daddy',
 });
+
+// const connection = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '',
+//     database: 'my_database',
+// });
 
 export default connection;
