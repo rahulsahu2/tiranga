d86f91237d2d78812d9226248f8f52174ff341a6
Rahul Sahu <rahulsa667@gmail.com>
2025-02-05 19:59:59 +0530

    db config

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,18 @@
 import mysql from 'mysql2/promise';
 
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tiranga',
-});
-
 // const connection = mysql.createPool({
-//     host: '193.203.184.165',
-//     user: 'u936826252_daddy',
-//     password: 'H@>M=5>aP5',
-//     database: 'u936826252_daddy',
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'tiranga',
 // });
+
+const connection = mysql.createPool({
+    host: '193.203.184.165',
+    user: 'u936826252_daddy',
+    password: 'H@>M=5>aP5',
+    database: 'u936826252_daddy',
+});
 
 // const connection = mysql.createPool({
 //     host: '127.0.0.1',
